50d47d7527b009614fa4194bcccb6cccda0c2373
IgoorToorres <igorevangelista121@gmail.com>
2026-02-13 14:17:37 -0300

    feat - modal de cadastro

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { AppointmentForm } from '@/components/appointment-form';
 import { PeriodSection } from '@/components/period-section';
 import { APPOINTMENTS_DATA, groupAppointmentByPeriod } from '@/utils';
 
@@ -20,6 +21,10 @@ export default function Home() {
         {periods.map((period, index) => (
           <PeriodSection period={period} key={index} />
         ))}
+      </div>
+
+      <div>
+        <AppointmentForm />
       </div>
     </div>
   );
